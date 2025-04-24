be3fe73290723d344d56e8e3843ea2d952e6e27b
unnawut <unnawut@unnawut.com>
2025-04-24 16:04:25 +0700

    feat: add SP1

---
+++ sp1/program/src/main.rs
@@ -0,0 +1,12 @@
+// These two lines are necessary for the program to properly compile.
+//
+// Under the hood, we wrap your main function with some extra code so that it behaves properly
+// inside the zkVM.
+#![no_main]
+sp1_zkvm::entrypoint!(main);
+
+use playground_common::scenarios::print_usize;
+
+pub fn main() {
+    print_usize::run();
+}
