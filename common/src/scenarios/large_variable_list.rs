ce75dfed5485557168a7f673a0b4458f30f64c4f
Unnawut Leepaisalsuwanna <921194+unnawut@users.noreply.github.com>
2025-04-29 16:42:35 +0700

    feat: add 2^31 and 2^32 to the comparison (#3)

--- common/src/scenarios/large_variable_list.rs
+++ common/src/scenarios/large_variable_list.rs
@@ -1,5 +1,5 @@
 use ssz_types::{
-    typenum::{U1073741824, Unsigned},
+    typenum::{Unsigned, U2147483648, U4294967296, U1099511627776},
     VariableList
 };
 
@@ -9,11 +9,20 @@ pub struct LargeVariableList;
 
 impl Scenario for LargeVariableList {
     fn name() -> String {
-        "large_merkle".to_string()
+        "large_variable_list".to_string()
     }
 
     fn run() {
-        println!("U1073741824::to_usize(): {}", U1073741824::to_usize());
-        println!("VariableList::<u8, U1073741824>::max_len(): {}", VariableList::<u8, U1073741824>::max_len());
+        // 2^31
+        println!("U2147483648::to_usize(): {}", U2147483648::to_usize());
+        println!("VariableList::<u8, U2147483648>::max_len(): {}", VariableList::<u8, U2147483648>::max_len());
+
+        // 2^32
+        println!("U4294967296::to_usize(): {}", U4294967296::to_usize());
+        println!("VariableList::<u8, U4294967296>::max_len(): {}", VariableList::<u8, U4294967296>::max_len());
+
+        // 2^40
+        println!("U1099511627776::to_usize(): {}", U1099511627776::to_usize());
+        println!("VariableList::<u8, U1099511627776>::max_len(): {}", VariableList::<u8, U1099511627776>::max_len());
     }
 }
